3b063f4246833aea283253ede6e8ff0e39a40c0c
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2024-01-16 00:20:01 +0530

    removing some packages and add a scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="">
+      <section className="h-[100vh] snap-center ">
+        <a href="#" className=" text-inherit">Link</a>
+      </section>
+      <section className="h-[100vh] snap-center ">Parallax</section>
+      <section className="h-[100vh] snap-center ">Service</section>
+      <section className="h-[100vh] snap-center ">Parallelax</section>
+      <section className="h-[100vh] snap-center ">Portfolio1</section>
+      <section className="h-[100vh] snap-center ">Portfolio2</section>
+      <section className="h-[100vh] snap-center ">Poertfoil3</section>
+      <section className="h-[100vh] snap-center ">Contact</section>
+    </div>
+  );
+};
 
-export default App
+export default App;
