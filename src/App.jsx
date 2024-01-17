3dbf72b70773d0965e1280645dfd9ef7d299f9e7
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2024-01-18 00:35:31 +0530

    learning framer motin

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Test from "./components/Test";
 
 const App = () => {
   return (
     <div className="">
-      <section className="h-[100vh] snap-center ">
+      {/* <section className="h-[100vh] snap-center ">
         <Navbar />
-        {/* <a href="#" className=" text-inherit">Link</a> */}
-      </section>
-      <section className="h-[100vh] snap-center ">Parallax</section>
+      </section> */}
+      {/* <section className="h-[100vh] snap-center ">Parallax</section>
       <section className="h-[100vh] snap-center ">Service</section>
       <section className="h-[100vh] snap-center ">Parallelax</section>
       <section className="h-[100vh] snap-center ">Portfolio1</section>
       <section className="h-[100vh] snap-center ">Portfolio2</section>
       <section className="h-[100vh] snap-center ">Poertfoil3</section>
-      <section className="h-[100vh] snap-center ">Contact</section>
+      <section className="h-[100vh] snap-center ">Contact</section> */}
+      <Test />
+      <Test />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='h-[100px]'>
+    <div className="h-[100px]">
       {/* Sidebar */}
       <div className="max-w-[1000px] m-auto flex items-center justify-between h-full">
         <span className="font-bold">Lama Dev</span>
